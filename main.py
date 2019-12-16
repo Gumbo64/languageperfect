ec197cec863c45130bbadd54bd27f8fe2dd68a7e
Rory <chickennuggetcrazykid@gmail.com>
2019-12-16 12:01:20 +1100

    a

--- main.py
+++ main.py
@@ -3,28 +3,30 @@ import pytesseract
 import pyautogui
 import time
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+#pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 translator = Translator()
 newtext = ''
 oldtext = ''
 dictionary = {}
 time.sleep(4)
 while True:
-    time.sleep(2)
-    image = pyautogui.screenshot(region=(748,752, 1800, 200))#
+    time.sleep(1.5)
+    image = pyautogui.screenshot(region=(288, 420, 1000, 200))#region=(748,752, 1800, 200)
     image.save("epic.png")
     oldtext= newtext
     newtext = pytesseract.image_to_string(image)
     if newtext[:11:] == 'Your answer':
-        image = pyautogui.screenshot(region=(1024,696, 928, 75))#
+        image = pyautogui.screenshot(region=(480, 369, 928, 75))#region=(1024,696, 928, 75)
         image.save("epic.png")
         answer = pytesseract.image_to_string(image)
-        translated = dictionary[newtext]
         dictionary[oldtext] = answer
     else:
         try:
             translated = dictionary[newtext]
         except:
             translated = str(translator.translate(newtext).text)
+        if translated == '':
+            translated = 'a'
+        pyautogui.hotkey('ctrl', 'a')
         pyautogui.typewrite(translated)
     pyautogui.press('enter')
