745f6b226e7af1f0eefc4e129d7e262421fe4e3b
Rory <chickennuggetcrazykid@gmail.com>
2019-12-16 18:13:00 +1100

    yee

--- main.py
+++ main.py
@@ -7,22 +7,32 @@ import time
 translator = Translator()
 newtext = ''
 oldtext = ''
+checktext = ''
 dictionary = {}
+check = False
 time.sleep(4)
 while True:
     time.sleep(1.5)
-    image = pyautogui.screenshot(region=(288, 420, 1000, 200))#region=(748,752, 1800, 200)
+    image = pyautogui.screenshot(region=(12, 398, 1382, 140))#region=(748,752, 1800, 200)
     image.save("epic.png")
     oldtext= newtext
     newtext = pytesseract.image_to_string(image)
     if newtext[:11:] == 'Your answer':
-        image = pyautogui.screenshot(region=(480, 369, 928, 75))#region=(1024,696, 928, 75)
-        image.save("epic.png")
+        if check == True:
+            try:
+                dictionary.pop(checktext)
+                check == False
+            except:
+                pass
+        image = pyautogui.screenshot(region=(455, 359, 600, 95))#region=(1024,696, 928, 75)
+        image.save("epic1.png")
         answer = pytesseract.image_to_string(image)
         dictionary[oldtext] = answer
     else:
         try:
             translated = dictionary[newtext]
+            check = True
+            checktext = newtext
         except:
             translated = str(translator.translate(newtext).text)
         if translated == '':
