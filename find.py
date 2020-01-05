e6e0f055e5dc8d8b918e284f42b1231c597cbe0e
Gumbo64 <chickennuggetcrazykid@gmail.com>
2020-01-05 20:14:39 +1100

    j

--- find.py
+++
@@ -1,6 +0,0 @@
-import pyautogui
-import time
-#prints the y position to be used as ypos in main.py. Put cursor slightly under the bookmarks (fullscreen) and copy that y position to ypos.
-while True:
-    time.sleep(2)
-    print(pyautogui.position().y)
